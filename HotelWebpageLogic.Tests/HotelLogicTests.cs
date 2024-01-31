59d1ac83349db62aca2dfa1999f18ced5a11a246
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2024-01-31 18:01:26 +0100

    Initial commit

---
+++ HotelWebpageLogic.Tests/HotelLogicTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using HotelWebsite.Data;
+using HotelWebsite.Logic;
+
+namespace HotelWebpageLogic.Tests
+{
+    public class HotelLogicTests
+    {
+        [Fact]
+        public void GetRooms_ShouldReturnListOfHotelModel()
+        {
+            // Given
+            HotelLogic logic = new();
+
+
+            // When
+            var returnedList = logic.GetRooms();
+
+            // Then
+            returnedList.Should().NotBeEmpty();
+            returnedList.Should().BeOfType<List<HotelRoomModel>>();
+        }
+    }
+}
---
+++ HotelWebsite.Data/HotelRoomModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelWebsite.Data
+{
+    public class HotelRoomModel
+    {
+        public int RoomNumber { get; set; }
+        public bool IsVacant { get; set; }
+        public int PricePerDay { get; set; }
+        public string RoomType { get; set; }
+        public int? BookedDaysLeft { get; set; }
+    }
+}
